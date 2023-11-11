0c5ed3fd370cb50d45a858d4da63fd2c706346a3
Therdsakmax050941 <maxza323@hotmail.co.th>
2023-11-11 16:00:19 +0700

    update Gif

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,29 +24,6 @@ function resetTransactions() {
     updateBalance();
 }
 
-function updateTransactions() {
-    const transactionList = document.getElementById('transactionList');
-    transactionList.innerHTML = '';
-
-    transactions.forEach((transaction, index) => {
-        const listItem = document.createElement('li');
-        const sign = transaction.type === 'income' ? '+' : '-';
-        const textColor = transaction.type === 'income' ? 'green' : 'red';
-
-        const description = transaction.type === 'income' ? `รายรับ` : `รายจ่าย`;
-        const formattedDesc = `#${index + 1} ${description} - ${transaction.description}`;
-        const text = `${sign} ${transaction.amount} ${formattedDesc}`;
-
-        listItem.textContent = text;
-        listItem.style.color = textColor;
-        listItem.classList.add('fade-in');
-
-        transactionList.appendChild(listItem);
-    });
-
-    showTransactionsWithFade();
-}
-
 function updateBalance() {
     totalBalance = transactions.reduce((total, transaction) => {
         return transaction.type === 'income' ? total + transaction.amount : total - transaction.amount;
@@ -86,11 +63,26 @@ function updateTransactions() {
         listItem.style.color = textColor;
         listItem.classList.add('fade-in');
 
+        // สร้างปุ่มลบ
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'X';
+        deleteButton.classList.add('btn', 'btn-danger', 'ml-2');
+        deleteButton.onclick = function() {
+            deleteTransaction(index); // เรียกใช้ฟังก์ชัน deleteTransaction เมื่อปุ่มถูกคลิก
+        };
+        listItem.appendChild(deleteButton);
+
         transactionList.appendChild(listItem);
     });
 
     showTransactionsWithFade(); 
 }
+function deleteTransaction(index) {
+    transactions.splice(index, 1); // ลบรายการที่ถูกกดปุ่มลบออกจากอาเรย์ transactions
+    updateTransactions(); // อัพเดทรายการหลังการลบ
+    updateBalance(); // อัพเดทยอดเงินคงเหลือ
+}
+
 function addTransaction(type) {
     const amount = parseFloat(document.getElementById('amount').value);
     const description = document.getElementById('description').value;
@@ -105,10 +97,26 @@ function addTransaction(type) {
         transactions.push(transaction);
         updateTransactions();
         updateBalance();
+        showGif();
     }
 
     document.getElementById('amount').value = ''; 
     document.getElementById('description').value = ''; 
+}
+
+function showGif() {
+    const totalBalance = parseInt(document.getElementById('totalBalance').textContent.split(': ')[1]);
+    const gifElement = document.getElementById('gif');
+
+    if (totalBalance <= 0) {
+        gifElement.src = ''; 
+    } else if (totalBalance < 3000) {
+        gifElement.src = './assets/imgs/plshelp.gif'; 
+    } else if (totalBalance < 8000) {
+        gifElement.src = './assets/imgs/im-fine.gif'; 
+    } else if (totalBalance > 10000) {
+        gifElement.src = './assets/imgs/Rich.gif'; 
+    }
 }
 
 
